602759205df142300652ecb30251464eaa1d8687
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-07-15 16:25:47 +0400

    Свёрстан футер и сделан адаптив
    Свёрстан футер и сделан адаптив

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let mySwiper = new Swiper('.swiper-container', {
   
     loop: true,
-    slidesPerView: 2,
+    slidesPerView: 1,
 
     navigation: {
         nextEl: '.button-next',
     },
+
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        }
+    }
 })
